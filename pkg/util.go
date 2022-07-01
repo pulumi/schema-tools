98ab419d5327b67640855e90fd57ee44b19c384d
Josh Kodroff <josh@pulumi.com>
2022-06-30 21:55:48 -0400

    Change downloadSchema to return an error instead of panic.

---
+++ pkg/util.go
@@ -0,0 +1,28 @@
+package pkg
+
+import (
+	"encoding/json"
+	"github.com/pulumi/pulumi/pkg/v3/codegen/schema"
+	"io/ioutil"
+	"net/http"
+)
+
+func DownloadSchema(schemaUrl string) (schema.PackageSpec, error) {
+	resp, err := http.Get(schemaUrl)
+	if err != nil {
+		return schema.PackageSpec{}, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return schema.PackageSpec{}, err
+	}
+
+	var sch schema.PackageSpec
+	if err = json.Unmarshal(body, &sch); err != nil {
+		return schema.PackageSpec{}, nil
+	}
+
+	return sch, nil
+}
