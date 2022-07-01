45f64eb4b800d8b2b0f379fb388555dda9595339
Josh Kodroff <josh@pulumi.com>
2022-06-30 21:55:48 -0400

    Change downloadSchema to return an error instead of panic.

---
+++ pkg/stats_test.go
@@ -0,0 +1,42 @@
+package pkg
+
+import (
+	"github.com/pulumi/pulumi/pkg/v3/codegen/schema"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCountStats(t *testing.T) {
+	schema := schema.PackageSpec{
+		Functions: map[string]schema.FunctionSpec{
+			"test:index/getFoo:getFoo": {
+				Description: "0123456789",
+			},
+			"test:index/getBar:getBar": {
+				Description: "01234",
+			},
+		},
+		Resources: map[string]schema.ResourceSpec{
+			"test:index/foo:Foo": {
+				InputProperties: map[string]schema.PropertySpec{
+					"noDesc1": {
+						Description: "",
+					},
+					"noDesc2": {
+						Description: "",
+					},
+					"hasDesc": {
+						Description: "What it does",
+					},
+				},
+			},
+		},
+	}
+
+	stats := CountStats(schema)
+
+	assert.Equal(t, 3, stats.TotalResourceInputs)
+	assert.Equal(t, 2, stats.ResourceInputsMissingDesc)
+	assert.Equal(t, 1, stats.TotalResources)
+	assert.Equal(t, 2, stats.TotalFunctions)
+}
