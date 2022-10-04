a30a7a74397ff2d61c9d02a4baa00c573e554fc3
Josh Kodroff <jkodroff@users.noreply.github.com>
2022-10-04 15:55:08 -0400

    Expand metrics, add tests. (#5)
    * Change downloadSchema to return an error instead of panic.
    * Change loadLocalPackageSpec to return an error instead of panic.
    * Remove unused code, fix error handling.
    * Add tests, makefile.
    * Add tests that count internal references.

--- pkg/stats_test.go
+++ pkg/stats_test.go
@@ -11,13 +11,80 @@ func TestCountStats(t *testing.T) {
 		Functions: map[string]schema.FunctionSpec{
 			"test:index/getFoo:getFoo": {
 				Description: "0123456789",
+				Inputs: &schema.ObjectTypeSpec{
+					Properties: map[string]schema.PropertySpec{
+						"input1": {
+							Description: "0123456789",
+						},
+						"input2": {
+							Description: "0",
+						},
+						"inputMissingDesc1": {},
+						"inputMissingDesc2": {},
+					},
+				},
+				Outputs: &schema.ObjectTypeSpec{
+					Properties: map[string]schema.PropertySpec{
+						"output1": {
+							Description: "0123456789",
+						},
+						"output2": {
+							Description: "01",
+						},
+						"outputMissingDesc1": {},
+						"outputMissingDesc2": {},
+						"outputMissingDesc3": {},
+					},
+				},
 			},
 			"test:index/getBar:getBar": {
-				Description: "01234",
+				Description: "0",
+				Inputs: &schema.ObjectTypeSpec{
+					Properties: map[string]schema.PropertySpec{
+						"input1": {
+							Description: "0",
+						},
+						"inputMissingDesc3": {},
+					},
+				},
+				Outputs: &schema.ObjectTypeSpec{
+					Properties: map[string]schema.PropertySpec{
+						"output1": {
+							Description: "0",
+						},
+						"outputMissingDesc4": {},
+					},
+				},
 			},
 		},
 		Resources: map[string]schema.ResourceSpec{
 			"test:index/foo:Foo": {
+				ObjectTypeSpec: schema.ObjectTypeSpec{
+					Description: "0123456789",
+					Properties: map[string]schema.PropertySpec{
+						"output1": {
+							Description: "0123456789",
+						},
+						"output2": {
+							Description: "01234",
+						},
+						"noDesc1": {
+							Description: "",
+						},
+						"noDesc2": {
+							Description: "",
+						},
+						"noDesc3": {
+							Description: "",
+						},
+						"noDesc4": {
+							Description: "",
+						},
+						"noDesc5": {
+							Description: "",
+						},
+					},
+				},
 				InputProperties: map[string]schema.PropertySpec{
 					"noDesc1": {
 						Description: "",
@@ -35,8 +102,129 @@ func TestCountStats(t *testing.T) {
 
 	stats := CountStats(testSchema)
 
-	assert.Equal(t, 3, stats.TotalResourceInputs)
-	assert.Equal(t, 2, stats.ResourceInputsMissingDesc)
-	assert.Equal(t, 1, stats.TotalResources)
-	assert.Equal(t, 2, stats.TotalFunctions)
+	// The example schema above is designed to give a unique number for each property as much as possible to provide
+	// the highest-confidence test results.
+	assert.Equal(t, 1, stats.Resources.TotalResources)
+	assert.Equal(t, 10, stats.Resources.TotalDescriptionBytes)
+
+	assert.Equal(t, 3, stats.Resources.TotalInputProperties)
+	assert.Equal(t, 2, stats.Resources.InputPropertiesMissingDescriptions)
+
+	assert.Equal(t, 7, stats.Resources.TotalOutputProperties)
+	assert.Equal(t, 5, stats.Resources.OutputPropertiesMissingDescriptions)
+
+	assert.Equal(t, 2, stats.Functions.TotalFunctions)
+	assert.Equal(t, 11, stats.Functions.TotalDescriptionBytes)
+
+	assert.Equal(t, 3, stats.Functions.InputPropertiesMissingDescriptions)
+	assert.Equal(t, 12, stats.Functions.TotalInputPropertyDescriptionBytes)
+
+	assert.Equal(t, 4, stats.Functions.OutputPropertiesMissingDescriptions)
+	assert.Equal(t, 13, stats.Functions.TotalOutputPropertyDescriptionBytes)
+}
+
+func TestCountStats_InternalRefs_Inputs(t *testing.T) {
+	testSchema := schema.PackageSpec{
+		Types: map[string]schema.ComplexTypeSpec{
+			"test:index/myType:myType": {
+				ObjectTypeSpec: schema.ObjectTypeSpec{
+					Properties: map[string]schema.PropertySpec{
+						"myProperty": {
+							TypeSpec: schema.TypeSpec{
+								Type: "integer",
+							},
+						},
+					},
+				},
+			},
+		},
+		Resources: map[string]schema.ResourceSpec{
+			"test:index/myResource:/myResource": {
+				InputProperties: map[string]schema.PropertySpec{
+					"myProperty": {
+						TypeSpec: schema.TypeSpec{
+							Ref: "#/types/test:index/myType:myType",
+						},
+					},
+					"myProperty2": {
+						TypeSpec: schema.TypeSpec{
+							Ref: "#/types/test:index/myType:myType",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	stats := CountStats(testSchema)
+
+	assert.Equal(t, 3, stats.Resources.TotalInputProperties)
+}
+
+func TestCountStats_InternalRefs_Outputs(t *testing.T) {
+	testSchema := schema.PackageSpec{
+		Types: map[string]schema.ComplexTypeSpec{
+			"test:index/myType:myType": {
+				ObjectTypeSpec: schema.ObjectTypeSpec{
+					Properties: map[string]schema.PropertySpec{
+						"myProperty": {
+							TypeSpec: schema.TypeSpec{
+								Type: "integer",
+							},
+						},
+					},
+				},
+			},
+		},
+		Resources: map[string]schema.ResourceSpec{
+			"test:index/myResource:/myResource": {
+				ObjectTypeSpec: schema.ObjectTypeSpec{
+					Description: "0123456789",
+					Properties: map[string]schema.PropertySpec{
+						"myProperty": {
+							TypeSpec: schema.TypeSpec{
+								Ref: "#/types/test:index/myType:myType",
+							},
+						},
+						"myProperty2": {
+							TypeSpec: schema.TypeSpec{
+								Ref: "#/types/test:index/myType:myType",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	stats := CountStats(testSchema)
+
+	assert.Equal(t, 3, stats.Resources.TotalOutputProperties)
+}
+
+func TestCountStats_ExternalRef(t *testing.T) {
+	testSchema := schema.PackageSpec{
+		Resources: map[string]schema.ResourceSpec{
+			"awsx:cloudtrail:Trail": {
+				InputProperties: map[string]schema.PropertySpec{
+					"bucket": {
+						TypeSpec: schema.TypeSpec{
+							Ref: "/aws/v5.4.0/schema.json#/resources/aws:s3%2Fbucket:Bucket",
+						},
+						Description: "The managed S3 Bucket where the Trail will place its logs.",
+					},
+				},
+			},
+		},
+	}
+
+	stats := CountStats(testSchema)
+
+	// We are mostly testing that we did not get a panic because of the external type ref
+	assert.Equal(t, stats.Resources.TotalResources, 1)
+	assert.Equal(t, stats.Resources.TotalInputProperties, 1)
+}
+
+func TestVersionlessName(t *testing.T) {
+	assert.Equal(t, "config:assumeRoleWithWebIdentity", versionlessName("#/types/aws:config/assumeRoleWithWebIdentity:assumeRoleWithWebIdentity"))
 }
