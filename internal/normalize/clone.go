70b630d175276eed66889cb5d3ec20aa3d404046
schema_tools/crew/fiddler <43035978+corymhall@users.noreply.github.com>
2026-02-24 07:37:22 -0500

    Use maps.Clone for normalization package snapshots

--- internal/normalize/clone.go
+++ internal/normalize/clone.go
@@ -1,24 +1,17 @@
 package normalize
 
-import "github.com/pulumi/pulumi/pkg/v3/codegen/schema"
+import (
+	"maps"
+
+	"github.com/pulumi/pulumi/pkg/v3/codegen/schema"
+)
 
 // clonePackageSpec creates a copy-on-write snapshot for maps that Normalize may
 // mutate so callers do not observe in-place map updates.
 func clonePackageSpec(spec schema.PackageSpec) schema.PackageSpec {
 	cloned := spec
-	cloned.Resources = cloneMap(spec.Resources)
-	cloned.Functions = cloneMap(spec.Functions)
-	cloned.Types = cloneMap(spec.Types)
+	cloned.Resources = maps.Clone(spec.Resources)
+	cloned.Functions = maps.Clone(spec.Functions)
+	cloned.Types = maps.Clone(spec.Types)
 	return cloned
-}
-
-func cloneMap[T any](m map[string]T) map[string]T {
-	if m == nil {
-		return nil
-	}
-	out := make(map[string]T, len(m))
-	for k, v := range m {
-		out[k] = v
-	}
-	return out
 }
